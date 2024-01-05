e441ae6ba919311f0e7d4b0a67b471a3a9d81a06
KevinXutie <kevincorpin40@gmail.com>
2024-01-05 18:35:33 +0800

    message

--- src/pages/bernard.tsx
+++ src/pages/bernard.tsx
@@ -6,73 +6,99 @@ import Image from "next/image";
 
 const Bernard = () => {
 	return (
-		<main>
-			<div className="w-full mx-auto flex flex-col gap-10 mb-5 fade-in-text lg:px-0">
-				<div className="w-full mx-auto flex flex-row items-center gap-4">
-					<div className="w-fit rounded-lg">
-						<Link
-							href="/portfolio"
-							className={buttonVariants({ variant: "ghost", size: "sm" })}
-						>
-							<ArrowLeft />
-						</Link>
-					</div>
-					<div>
-						<div className="w-full text-3xl flex flex-row gap-2">
-							<p className="font-bold">Bernard Turiana</p>
-							<Badge className="bg-gradient-to-r from-yellow-200 to bg-pink-400 outline-none border-none font-bold">
-								PC ASSEMBLY
-							</Badge>
+		<main className="w-full mx-auto flex flex-col justify-center">
+			<div className="flex flex-col">
+				<div className="w-full mx-auto flex flex-col items-center gap-10 mb-5 fade-in-text">
+					<div className="w-full mx-auto flex flex-row items-center gap-4">
+						<div className="w-fit rounded-lg">
+							<Link
+								href="/portfolio"
+								className={buttonVariants({ variant: "ghost", size: "sm" })}
+							>
+								<ArrowLeft />
+							</Link>
 						</div>
-						<div>October 21, 2023</div>
+						<div>
+							<div className="w-full text-3xl flex md:flex-row gap-2 items-center">
+								<div className="font-bold">Bernard Turiana</div>
+								<div>
+									<Badge className="bg-gradient-to-r from-yellow-200 to bg-pink-400 outline-none border-none font-bold p-2 text-center">
+										PC ASSEMBLY
+									</Badge>
+								</div>
+							</div>
+							<div>October 21, 2023</div>
+						</div>
+					</div>
+					<div className="flex flex-col text-center sm:text-left">
+						<div>
+							Hey there! Just wanted to share the awesome project I recently
+							wrapped up for my client, Bernard Turiana! I put together a killer
+							personal computer for him, working within the budget he had on
+							hand. It was a blast bringing his vision to life, and I&apos;m
+							stoked with how it turned out. If you&apos;re into tech stuff, you
+							should definitely take a peek!
+						</div>
+						<div>
+							<h3 className="text-xl py-4 font-bold">
+								Project: Crafting the Ultimate Personal Computer for Client
+								Bernard Turiana within Budget Constraints.
+							</h3>
+							<div className="flex flex-col m-auto gap-5">
+								<li>
+									Ryzen 5 5600G, 6 Cores 12 threads Base Clock 3.9GHz Max. Boost
+									Clock Up to 4.4GHz
+								</li>
+								<li> MSI B450-A Pro Max</li>
+								<li> 16gb 8x2 Ram TFroce 3200Mhz</li>
+								<li> TreadSonic Case</li>
+								<li> Inplay 650W Ultra Series GS650 80plus bronze</li>
+								<li> 512gb 2x2 GX2 SSD TeamGroup</li>
+								<li> 24 inch Frameless Monitor NVISION</li>
+							</div>
+						</div>
 					</div>
 				</div>
-				<p>
-					Hey there! Just wanted to share the awesome project I recently wrapped
-					up for my client, Bernard Turiana! I put together a killer personal
-					computer for him, working within the budget he had on hand. It was a
-					blast bringing his vision to life, and I&apos;m stoked with how it
-					turned out. If you&apos;re into tech stuff, you should definitely take
-					a peek!
-				</p>
-				<div>
-					<h3 className="text-xl mb-5 fontbo">
-						Project: Crafting the Ultimate Personal Computer for Client Bernard
-						Turiana within Budget Constraints.
-					</h3>
-					<div className="flex flex-col m-auto gap-5">
-						<h3 className="font-semibold">Adding Components Specifications</h3>
-						<li>
-							Ryzen 5 4650G, 6 Cores 12 threads Base Clock 3.7GHz Max. Boost
-							Clock Up to 4.2GHz
-						</li>
-						<li> Gigabyte GeForce GTX 1650</li>
-						<li> 16gb 8x2 Ram TFroce 3200Mhz</li>
-						<li> DarkFlash Twisted Ver 2.6 Liquid Cooling</li>
-						<li> 512gb 2x2 GX2 SSD TeamGroup</li>
-						<li> 24 inch Curve ATLAS Gamdias</li>
+				<div className="grid sm:grid-cols-2 grid-cols-1 gap-5 lg:col-span-2">
+					<div className="w-fit animate-in bg-secondary mx-auto rounded-xl p-2 hover:bg-accent">
+						<div className="relative w-[300px] h-[300px]">
+							<Image
+								src="/bernard1.png"
+								fill
+								className="object-contain"
+								alt=""
+							/>
+						</div>
 					</div>
-				</div>
-			</div>
-			<div className=" max-h-max fade-in-image rounded-xl px-5 py-5 grid gap-5 lg:grid-cols-2 md:grid-cols-1 ">
-				<div className="w-fit px-5 py-5 animate-in bg-secondary mx-auto rounded-xl hover:bg-accent">
-					<div className="relative w-[400px] h-[400px]">
-						<Image src="/bernard1.png" fill className="object-contain" alt="" />
+					<div className="w-fit animate-in bg-secondary mx-auto rounded-xl p-2 hover:bg-accent">
+						<div className="relative w-[300px] h-[300px]">
+							<Image
+								src="/bernard2.png"
+								fill
+								className="object-contain"
+								alt=""
+							/>
+						</div>
 					</div>
-				</div>
-				<div className="w-fit px-5 py-5 animate-in bg-secondary mx-auto rounded-xl hover:bg-accent">
-					<div className="relative w-[400px] h-[400px]">
-						<Image src="/bernard2.png" fill className="object-contain" alt="" />
+					<div className="w-fit animate-in bg-secondary mx-auto rounded-xl p-2 hover:bg-accent">
+						<div className="relative w-[300px] h-[300px]">
+							<Image
+								src="/bernard3.png"
+								fill
+								className="object-contain"
+								alt=""
+							/>
+						</div>
 					</div>
-				</div>
-				<div className="w-fit px-5 py-5 animate-in bg-secondary mx-auto rounded-xl hover:bg-accent">
-					<div className="relative w-[400px] h-[400px]">
-						<Image src="/bernard3.png" fill className="object-contain" alt="" />
-					</div>
-				</div>
-				<div className="w-fit px-5 py-5 animate-in bg-secondary mx-auto rounded-xl hover:bg-accent">
-					<div className="relative w-[400px] h-[400px]">
-						<Image src="/bernard4.png" fill className="object-contain" alt="" />
+					<div className="w-fit animate-in bg-secondary mx-auto rounded-xl p-2 hover:bg-accent">
+						<div className="relative w-[300px] h-[300px]">
+							<Image
+								src="/bernard4.png"
+								fill
+								className="object-contain"
+								alt=""
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
