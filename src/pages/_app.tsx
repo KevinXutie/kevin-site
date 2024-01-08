3f2cca8b701ab171a22d0b7383d384dc54fa4743
KevinXutie <kevincorpin40@gmail.com>
2024-01-08 23:11:34 +0800

    message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,26 @@
-import '@/styles/globals.css'
-import Page from '@/components/page-wrapper'
-import type { AppProps } from 'next/app'
-import NavBar from '../components/navbar'
-import NextThemeProvider from '../components/theme-provider'
-import Footer from '../components/footer'
+import "@/styles/globals.css";
+
+import Page from "@/components/page-wrapper";
+import { Analytics } from "@vercel/analytics/react";
+import type { AppProps } from "next/app";
+import Footer from "../components/footer";
+import NavBar from "../components/navbar";
+import NextThemeProvider from "../components/theme-provider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-			<>
-				<NextThemeProvider>
-					<NavBar />
-					<div className="min-h-screen">
-						<Page>
-							<Component {...pageProps} />
-							<Footer />
-						</Page>
-					</div>
-				</NextThemeProvider>
-			</>
-		);
+	return (
+		<>
+			<NextThemeProvider>
+				<NavBar />
+				<div className="min-h-screen">
+					<Page>
+						<Component {...pageProps} />
+						<Footer />
+					</Page>
+				</div>
+			</NextThemeProvider>
+			<Analytics />
+		</>
+	);
 }
 
